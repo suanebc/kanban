3862ee4e7f76d7acd3ca434086c2c2f6b2524b27
suanebc <60412898+suanebc@users.noreply.github.com>
2026-04-21 10:45:12 -0300

    funções para criar, atualizar e deletar tarefas

--- script.js
+++ script.js
@@ -1,50 +1,88 @@
 
 const colunasKanba = document.querySelector(".colunas");
 const colunas = colunasKanba.querySelectorAll(".coluna");
+const modalDeletar = document.querySelectorAll(".coluna");
 
-//let tarefaAtual = null;
+let tarefaAtual = null;
+
+// FUNÇÕES ----------------------------------------------
+
+const deletarTarefa = (event) => {
+  tarefaAtual = event.target.closest(".tarefa");//encontrar o ancestral mais próximo que corresponda a um seletor CSS específico
+  modalDeletar.querySelector(".preview").innerText = tarefaAtual.innerText.substring(0, 100);
+  modalDeletar.showModal();
+
+}
+
+const editarTarefa = (event) => {
+  const tarefa = event.target.closest(".tarefa");
+  const input = criarTarefaInput(tarefa.innerText);
+  tarefa.replaceWith(input);
+  input.focus();
+
+  // mover cursos
+  const selecionar = window.getSelection();
+  selecionar.selectAllChildren(input)
+  selecionar.collapseToEnd();
+
+}
 const handleBlur = (event) => {
   const input = event.target;
   const content = input.innerText.trim() || "Untitled";
   const tarefa = criarTarefa(content.replace(/\n/g, "<br>"));
   input.replaceWith(tarefa);
 };
-const handleAdd = (event) => {
+// FUNÇÃO ADD TAREFA
+const addTarefa = (event) => {
   const tarefaElemento = event.target.closest(".coluna").lastElementChild;
   const input = criarTarefaInput();
   tarefaElemento.appendChild(input);
   input.focus();
 };
+
+// FUNÇÃO ATUALIZAR CONTADOR DE TAREFA
+const atualizarContador = (column) => {
+  const tarefa = column.querySelector(".tarefa").children;
+  const contadorTarefa = tarefa.length;
+  column.querySelector(".coluna-titulo h3").dataset.tarefa = contadorTarefa
+}
+// FUNÇÃO PARA CRIAR TAREFA
 const criarTarefa = (content) => {
-  const tarefa = document.createElement("div");
-  tarefa.className = "tarefa";
-  tarefa.draggable = true;
+  const tarefa = document.createElement("div"); // cia o elemento div
+  tarefa.className = "tarefa"; // cria classe tarefa
+  tarefa.draggable = true; // atributo para arrastar 
   tarefa.innerHTML = `
   <div>${content}</div>
   <menu>
       <button data-edit><i class="bi bi-pencil-square"></i></button>
       <button data-delete><i class="bi bi-trash"></i></button>
   </menu>`;
-  tarefa.addEventListener("dragstart", handleDragstart);
-  tarefa.addEventListener("dragend", handleDragend);
+  //tarefa.addEventListener("dragstart", handleDragstart);
+  //tarefa.addEventListener("dragend", handleDragend);
   return tarefa;
 };
+// FUNÇÃO PARA CRIAR INPUT
 const criarTarefaInput = (text = "") => {
   const input = document.createElement("div");
   input.className = "tarefa-input";
-  input.dataset.placeholder = "tarefa";
+  input.dataset.placeholder = "Nome da Tarefa";
   input.contentEditable = true;
   input.innerText = text;
   input.addEventListener("blur", handleBlur);
   return input;
 };
-
+// FUNÇÃO PARA CLICK NO BOTÃO +
 colunasKanba.addEventListener("click", (event) => {
   if (event.target.closest("button[data-add]")) {
-    handleAdd(event);
+    addTarefa(event);
   } else if (event.target.closest("button[data-edit]")) {
     handleEdit(event);
   } else if (event.target.closest("button[data-delete]")) {
     handleDelete(event);
   }
 });
+
+modalDeletar.addEventListener('submit', () => tarefaAtual && tarefaAtual.remove());
+modalDeletar.querySelector("#cancel").addEventListener("click", () => modalDeletar.close());
+
+modalDeletar.addEventListener("close", () => (tarefaAtual = null))
