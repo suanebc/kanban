3a78272146ee31ad5a193fd9e23414286f7676ae
suanebc <60412898+suanebc@users.noreply.github.com>
2026-04-25 12:37:19 -0300

    arrumnado css

---
+++ src/script.js
@@ -0,0 +1,5 @@
+
+const a = document.getElementById("year");
+a.innerText = new Date().getFullYear();
+const colunas = document.querySelector(".colunas");
+const coluna = colunas.querySelectorAll(".coluna");
